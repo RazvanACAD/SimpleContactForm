e49102fcff3f3b7e2628300552994eab95ea2120
Razvan <razvan@acad.ropardo.ro>
2024-04-01 11:58:49 +0300

    Instantiate a Plugin

--- simple-contact-form/simple-conact-form.php
+++ simple-contact-form/simple-conact-form.php
@@ -8,10 +8,40 @@
  * Text Domain: simple-contact-form
  */
 
- if( !defined('ABSPATH')){
+if( !defined('ABSPATH')){
  
     echo 'Hello';
     exit;
 
- }
+}
 
+class SimpleContactForm{
+
+    public function __construct(){
+
+        add_action('init', array($this, 'create_custom_post_type'));
+
+    }
+
+    public function create_custom_post_type(){
+
+        $args = array(
+
+            'public'=>true,
+            'has_archive'=> true,
+            'supports'=>array('title'),
+            'excluded_from_search'=>true,
+            'publicly_queryable'=>'manage_options',
+            'labels'=>array(
+
+                'name'=>'Contact Form',
+                'singular_name'=>'Contact Form Entry'
+            ),
+            'menu_icon'=> 'dashicons-media-text',
+        );
+
+        register_post_type('somple_contect_form', $args);
+    }
+}
+
+new SimpleContactForm;
