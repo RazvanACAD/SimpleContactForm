dfab3fd014693c477b039df5846bb448f59ae855
Razvan <razvan@acad.ropardo.ro>
2024-04-01 11:45:54 +0300

    first commit

---
+++ simple-contact-form/simple-conact-form.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Simple Contact Form
+ * Description: Simple contact form project
+ * Author: Razvan
+ * Version: 1.0.0
+ * Text Domain: simple-contact-form
+ */
+
+ if( !defined('ABSPATH')){
+ 
+    echo 'Hello';
+    exit;
+
+ }
+
